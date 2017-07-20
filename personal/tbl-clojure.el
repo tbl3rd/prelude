c24269b5f5dc83d1b58a9ae7423530e5bc224718
Tom Lyons <tbl@broadinstitute.org>
2017-07-20 18:53:13 -0400

    Bind M-j to sp-join-sexp.

--- personal/tbl-clojure.el
+++ personal/tbl-clojure.el
@@ -3,6 +3,7 @@
 (defun tbl-clojure-hook ()
   "Run this in any Clojure buffer."
   (smartparens-strict-mode t)
+  (local-set-key (kbd "M-j") 'sp-join-sexp)
   (aggressive-indent-mode))
 
 (defun tbl-cider-hook ()
