309f57712efa6acad183cc2f03916f8726ef861c
Tom Lyons <tbl@broadinstitute.org>
2018-10-30 10:39:14 -0400

    Helm JIRA hax.

--- personal/tbl-helm.el
+++ personal/tbl-helm.el
@@ -1,3 +1,10 @@
 (require 'prelude-helm-everywhere)
 
+'(require 'helm-jira)
+
+'(setq helm-jira-url "https://broadinstitute.atlassian.net"
+      helm-jira-user-name "tbl"
+      helm-jira-project "DSDEGP"
+      helm-jira-board-id 407)
+
 (provide 'tbl-helm)
