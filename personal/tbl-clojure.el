6374cb521b7bef31250f1003da89e42a7b64d0ea
Tom Lyons <tbl@broadinstitute.org>
2020-08-07 16:06:24 -0400

    Conform closer to Hornet defaults in Cursive.

--- personal/tbl-clojure.el
+++ personal/tbl-clojure.el
@@ -1,12 +1,14 @@
-'(require 'aggressive-indent)
+(require 'aggressive-indent)
+;; (require 'clj-refactor)
 
 (defun tbl-clojure-hook ()
   "Run this in any Clojure buffer."
-  (setq clojure-indent-style 'align-arguments)
+  (setq clojure-indent-style 'always-indent)
   (smartparens-strict-mode t)
   (local-set-key (kbd "M-j") 'sp-join-sexp)
-  ;; (aggressive-indent-mode 1)
-  )
+  (aggressive-indent-mode 1)
+  ;; (clj-refactor-mode 1)
+  (yas-minor-mode 1))
 
 (defun tbl-cider-hook ()
   "Run this in any Clojure buffer when CIDER is active."
@@ -21,11 +23,12 @@
 (add-hook 'cider-mode-hook      'tbl-cider-hook)
 (add-hook 'cider-repl-mode-hook 'tbl-cider-hook)
 
-(add-to-list 'auto-mode-alist '("\\.cljs.hl\\'" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.cljs.hl\\'" . clojurescript-mode))
 (add-to-list 'auto-mode-alist '("\\.boot\\'" . clojure-mode))
-(add-to-list 'auto-mode-alist '("\\.cljs\\'" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.cljs\\'" . clojurescript-mode))
 (add-to-list 'auto-mode-alist '("\\.clj\\'" . clojure-mode))
 
-(setq cljr-inject-dependencies-at-jack-in nil)
+'(setq cljr-inject-dependencies-at-jack-in nil)
+'(setq cider-default-cljs-repl 'shadow)
 
 (provide 'tbl-clojure)
